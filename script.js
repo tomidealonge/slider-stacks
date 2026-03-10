0d6010af002d855168e96b29e3239c39dc07265d
tomidealonge <akintomide.alonge@gmail.com>
2026-03-10 01:23:22 +0100

    chore: implement text-animation

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// import gsap from "gsap";
+// import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
 // steps for anime
 // map through all the images and reduce the scale and translate y values to have the jut up
 // monitor the wheel event and detect where the user wants to scroll to (top or bottom)
@@ -19,6 +23,7 @@ function getZIndex(nodeList, type = "highest") {
   );
 }
 document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(SplitText);
   const scaleFactor = 0.06;
   const translateYFactor = 40;
   let baseZIndex = 1147483647;
@@ -57,8 +62,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let scrollTimeout;
 
-  console.log('factorArray-initial', factorArray);
-
   window.addEventListener("wheel", (event) => {
     clearTimeout(scrollTimeout);
 
@@ -80,44 +83,77 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const activeSlide = stack.children[factorArray.indexOf(0)];
 
+        const activeSlideIndex = factorArray.indexOf(0);
+
         const removed = factorArray.pop();
 
         factorArray.unshift(removed);
 
+        const newActiveSlide = stack.children[factorArray.indexOf(0)];
+
         const newZIndex = getZIndex(stack.children, "lowest") - 1;
         
+
+        let wordsInstance = null;
+
         activeTL
+          .add(() => {
+            wordsInstance = SplitText.create(newActiveSlide.querySelector("h2"), {
+              type: "words, chars",
+              onSplit(self) {
+                gsap.set(self.words, {
+                  y: "30%",
+                  autoAlpha: 0,
+                });
+              },
+            });
+            
+          })
           .to(activeSlide, {
             y: 100,
-            scale: 1.06,
+            scale: 1.12,
             opacity: 0,
-            duration: 0.5,
+            duration: 0.8,
             ease: "power2.out",
           })
           .add(() => {
             Array.from(stack.children).forEach((slide, index) => {
+              if (slide === activeSlide) return;
               gsap.to(slide, {
                 scale: 1 - factorArray[index] * scaleFactor,
                 y: factorArray[index] * -translateYFactor,
-                duration: 0.5,
+                duration: 0.4,
                 ease: "power2.out",
               });
             });
           }, "<")
-          .set(
+          .set(activeSlide, {
+            zIndex: newZIndex,
+            scale: 1 - factorArray[activeSlideIndex] * scaleFactor,
+            duration: 0.1,
+          })
+          .fromTo(
             activeSlide,
             {
-              zIndex: newZIndex,
-            }
+              y: 0,
+            },
+            {
+              opacity: 1,
+              y: (factorArray.length - 1) * -translateYFactor,
+              duration: 0.2,
+              ease: "power2.inOut",
+            },
+            "<",
           )
-          .fromTo(activeSlide, {
-            opacity: 0,
-            y: 0,
-          }, {
-            opacity: 1,
-            y: (factorArray.length - 1) * -translateYFactor,
-            duration: 0.5,
-            ease: "power2.inOut",
+          .add(() => {
+            gsap.to(wordsInstance?.words, {
+              y: '0%',
+              autoAlpha: 1,
+              duration: 0.3,
+              stagger: 0.05,
+              ease: "power2.inOut",
+            })
+
           })
       } else {
         const activeTL = gsap.timeline({
@@ -126,7 +162,10 @@ document.addEventListener("DOMContentLoaded", () => {
           },
         });
 
-        const activeSlide = stack.children[factorArray.indexOf(factorArray.length - 1)];
+        let wordsInstance = null;
+
+        const activeSlide =
+          stack.children[factorArray.indexOf(factorArray.length - 1)];
 
         const removed = factorArray.shift();
 
@@ -135,6 +174,17 @@ document.addEventListener("DOMContentLoaded", () => {
         const newZIndex = getZIndex(stack.children, "highest") + 1;
 
         activeTL
+          .add(() => {
+            wordsInstance = SplitText.create(activeSlide.querySelector("h2"), {
+              type: "words, chars",
+              onSplit(self) {
+                gsap.set(self.words, {
+                  y: "30%",
+                  autoAlpha: 0,
+                });
+              },
+            });
+          })
           .to(activeSlide, {
             y: -100,
             opacity: 0,
@@ -151,12 +201,9 @@ document.addEventListener("DOMContentLoaded", () => {
               });
             });
           }, "<")
-          .set(
-            activeSlide,
-            {
-              zIndex: newZIndex,
-            }
-          )
+          .set(activeSlide, {
+            zIndex: newZIndex,
+          })
           .fromTo(
             activeSlide,
             { opacity: 0, y: translateYFactor },
@@ -165,8 +212,17 @@ document.addEventListener("DOMContentLoaded", () => {
               y: 0,
               duration: 0.5,
               ease: "power2.inOut",
-            }
-          );
+            },
+          )
+          .add(() => {
+            gsap.to(wordsInstance?.words, {
+              y: '0%',
+              autoAlpha: 1,
+              duration: 0.3,
+              stagger: 0.05,
+              ease: "power2.inOut",
+            })
+          })
       }
     }, 20);
   });
